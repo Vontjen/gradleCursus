b1be9c9dbdf810a8225b68f01af8d45e142fd062
Tjen Dekker <vontjen@gmail.com>
2017-07-03 10:44:05 +0200

    Commit from class

--- src/main/java/com/realdolmen/greeting/HelloService.java
+++ src/main/java/com/realdolmen/greeting/HelloService.java
@@ -4,4 +4,8 @@ package com.realdolmen.greeting;
  * Created by vdabcursist on 03/07/2017.
  */
 public class HelloService {
+
+    public String sayHello (String name){
+        return "Hello, " +name;
+    }
 }
--- src/main/java/com/realdolmen/greeting/Main.java
+++ src/main/java/com/realdolmen/greeting/Main.java
@@ -4,4 +4,8 @@ package com.realdolmen.greeting;
  * Created by vdabcursist on 03/07/2017.
  */
 public class Main {
+    public static void main(String[] args) {
+        HelloService hs = new HelloService();
+        hs.sayHello("Tjen");
+    }
 }
